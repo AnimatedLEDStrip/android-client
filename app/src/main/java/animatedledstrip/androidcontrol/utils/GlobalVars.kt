008dad861523a3dedf3b1ec8fea49995f7408650
Max Narvaez <mnmax.narvaez3@gmail.com>
2020-07-04 12:31:38 -0500

    Remove presetColors (now in core library)

--- app/src/main/java/animatedledstrip/androidcontrol/utils/GlobalVars.kt
+++ app/src/main/java/animatedledstrip/androidcontrol/utils/GlobalVars.kt
@@ -27,7 +27,6 @@ import android.widget.ArrayAdapter
 import android.widget.LinearLayout
 import animatedledstrip.animationutils.AnimationData
 import animatedledstrip.client.AnimationSender
-import animatedledstrip.colors.ccpresets.*
 
 // Configured IPs
 val IPs = mutableListOf<String>()
@@ -38,142 +37,9 @@ const val defaultPort = 6
 // Main Animation Sender
 var mainSender: AnimationSender = AnimationSender(ipAddress = "", port = defaultPort)
 
-var connected = false
-
 // AnimationData instance that will be sent and recreated as needed
 var animationData = AnimationData()
 
-// List of supported animations
-val animationOptions: MutableList<String>
-    get() = mainSender.supportedAnimations.keys.sorted().toMutableList()
-
 lateinit var animationOptionAdapter: ArrayAdapter<String>
-
-// Color presets
-val presetColors = listOf(
-    listOf<Long>(                   // RainbowColors from FastLED
-        0xFF0000,
-        0xD52A00,
-        0xAB5500,
-        0xAB7F00,
-        0xABAB00,
-        0x56D500,
-        0x00FF00,
-        0x00D52A,
-        0x00AB55,
-        0x0056AA,
-        0x0000FF,
-        0x2A00D5,
-        0x5500AB,
-        0x7F0081,
-        0xAB0055,
-        0xD5002B
-    ),
-    listOf(                         // CloudColors from FastLED
-        CCBlue.color,
-        CCDarkBlue.color,
-        CCDarkBlue.color,
-        CCDarkBlue.color,
-        CCDarkBlue.color,
-        CCDarkBlue.color,
-        CCDarkBlue.color,
-        CCDarkBlue.color,
-        CCBlue.color,
-        CCDarkBlue.color,
-        CCSkyBlue.color,
-        CCSkyBlue.color,
-        CCLightBlue.color,
-        CCWhite.color,
-        CCLightBlue.color,
-        CCSkyBlue.color
-    ),
-    listOf(                         // LavaColors from FastLED
-        CCBlack.color,
-        CCMaroon.color,
-        CCBlack.color,
-        CCMaroon.color,
-        CCDarkRed.color,
-        CCMaroon.color,
-        CCDarkRed.color,
-        CCDarkRed.color,
-        CCDarkRed.color,
-        CCRed.color,
-        CCOrange.color,
-        CCWhite.color,
-        CCOrange.color,
-        CCRed.color,
-        CCDarkRed.color
-    ),
-    listOf(                         // OceanColors from FastLED
-        CCMidnightBlue.color,
-        CCDarkBlue.color,
-        CCMidnightBlue.color,
-        CCNavy.color,
-        CCDarkBlue.color,
-        CCMediumBlue.color,
-        CCSeaGreen.color,
-        CCTeal.color,
-        CCCadetBlue.color,
-        CCBlue.color,
-        CCDarkCyan.color,
-        CCCornflowerBlue.color,
-        CCAquamarine.color,
-        CCSeaGreen.color,
-        CCAqua.color,
-        CCLightSkyBlue.color
-    ),
-    listOf(                         // ForestColors from FastLED
-        CCDarkGreen.color,
-        CCDarkGreen.color,
-        CCDarkOliveGreen.color,
-        CCDarkGreen.color,
-        CCGreen.color,
-        CCForestGreen.color,
-        CCOliveDrab.color,
-        CCGreen.color,
-        CCSeaGreen.color,
-        CCMediumAquamarine.color,
-        CCLimeGreen.color,
-        CCLawnGreen.color,
-        CCMediumAquamarine.color,
-        CCForestGreen.color
-    ),
-    listOf<Long>(                  // CCRainbowStripesColors from FastLED
-        0xFF0000,
-        0x000000,
-        0xAB5500,
-        0x000000,
-        0xABAB00,
-        0x000000,
-        0x00FF00,
-        0x000000,
-        0x00AB55,
-        0x000000,
-        0x0000FF,
-        0x000000,
-        0x5500AB,
-        0x000000,
-        0xAB0055,
-        0x000000
-    ),
-    listOf<Long>(                  // PartyColors from FastLED
-        0x5500AB,
-        0x84007C,
-        0xB5004B,
-        0xE5001B,
-        0xE81700,
-        0xB84700,
-        0xAB7700,
-        0xABAB00,
-        0xAB5500,
-        0xDD2200,
-        0xF2000E,
-        0xC2003E,
-        0x8F0071,
-        0x5F00A1,
-        0x2F00D0,
-        0x0007F9
-    )
-)
 
 fun LinearLayout?.indexOfChildOrNull(view: View?) = this?.indexOfChild(view)
